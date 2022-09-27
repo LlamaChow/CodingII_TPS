35258f0178b7e1491a05ad0d2580b3aec2cda67f
LlamaChow <76419385+LlamaChow@users.noreply.github.com>
2022-09-27 15:03:49 -0500

    Initial Commit
    This is the project at the time of creation. The project is in a stable state, and if we work slowly and carefully it will not break(If the gods find favor with us).

---
+++ Source/CodingII/CodingIIGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CodingIIGameMode.h"
+#include "CodingIICharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACodingIIGameMode::ACodingIIGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/CodingII/CodingIIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodingIIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACodingIIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACodingIIGameMode();
+};
+
+
+
