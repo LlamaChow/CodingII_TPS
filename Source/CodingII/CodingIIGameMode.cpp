5280713546778640d2ec675614f5ad73f31cebb8
LlamaChow <76419385+LlamaChow@users.noreply.github.com>
2022-12-16 12:12:48 -0600

    General Update
    Started laying down the foundation for the game. The first section of the play area has been laid out, might be shifted slightly in the future.

--- Source/CodingII/CodingIIGameMode.cpp
+++ Source/CodingII/CodingIIGameMode.cpp
@@ -7,7 +7,8 @@
 ACodingIIGameMode::ACodingIIGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> 
+	PlayerPawnBPClass(TEXT("/ Game / Scripts / Blueprints / BP_ThirdPersonCharacter.BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
